cec4c116b3cb3c6811d514fa943d5d2bddf6c13f
deep <officialdipchandan@gmail.com>
2023-08-29 20:43:45 +0545

    operators and variables

---
+++ pythonoperators.py
@@ -0,0 +1,22 @@
+
+
+"""
+
+Arithmetic operators
+Add:+ is also used for cancatination for example: "name"+"name"
+Sub: -
+Mul: * is also used for repetation of string for example "hello"*3
+Div: /
+Modulus: %
+Power **
+Floor Division://
+
+"""
+"""
+data type----
+Dictionary
+class name:dict
+def:dictionary is collection if items  arranged as key value separateed by commas and enclosed with curl/middle bracket
+d={"name":"deep","address":"siraha","age":"21"}
+
+"""
