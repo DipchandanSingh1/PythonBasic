3d239d27f1b7e25af773855446e77ea0fc91949e
deep <officialdipchandan@gmail.com>
2023-08-29 20:43:45 +0545

    operators and variables

---
+++ basic.py
@@ -0,0 +1,27 @@
+
+
+#topic: python comments
+
+# is used  for single line comments and multiline comments use trippl3 quote """___"""
+
+#for example
+""" this is my first program multiline comment
+in python and we are studying about comments today
+"""
+
+
+"""
+
+Topic: python variables.....
+Rules for python variables
+1. Variable name should start with a letter alphabet
+2.variable name cannot be start with number
+3. underscore symbol are allowed but not space is allowed in between variable name
+4. keyword are not allowed
+5. variable name should be descriptive
+
+"""
+
+
+
+
---
+++ pythonoperators.py
@@ -0,0 +1,14 @@
+
+
+"""
+
+Arithmetic operators
+Add:+ is also used for cancatination for example: "name"+"name"
+Sub: -
+Mul: * is also used for repetation of string for example "hello"*3
+Div: /
+Modulus: %
+Power **
+Floor Division://
+
+"""
